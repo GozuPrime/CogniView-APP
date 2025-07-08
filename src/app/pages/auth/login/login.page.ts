e6a2597555356917816ce5d066cb544c850add3c
Anthony Enrique Velasquez Honorio <anthony2000_honorio@outlook.com>
2025-07-08 09:05:42 -0500

    title pages

--- src/app/pages/auth/login/login.page.ts
+++ src/app/pages/auth/login/login.page.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonInput, IonLabel, IonItem, IonList, IonButton } from '@ionic/angular/standalone';
-import { ButtonComponent } from "src/app/shared/components/button/button.component";
 import { CardComponent } from "src/app/shared/components/card/card.component";
 import { SubtitleComponent } from "src/app/shared/components/subtitle/subtitle.component";
 import { FormLoginComponent } from "src/app/components/auth/form-login/form-login.component";
@@ -12,7 +10,7 @@ import { FormLoginComponent } from "src/app/components/auth/form-login/form-logi
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonContent, CommonModule, FormsModule, IonInput, IonLabel, IonItem, IonList, ButtonComponent, CardComponent, SubtitleComponent, FormLoginComponent]
+  imports: [ CommonModule, FormsModule, CardComponent, SubtitleComponent, FormLoginComponent]
 })
 export class LoginPage implements OnInit {
 
