65fd693914fd39b41a2c1ad0d77afc4d9d288524
Gozu <rbazalarca11@ucvvirtual.edu.pe>
2025-07-20 16:23:45 -0500

    form auth test

--- src/app/components/auth/form-login/form-login.component.ts
+++ src/app/components/auth/form-login/form-login.component.ts
@@ -1,17 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit, output } from '@angular/core';
 import { ButtonComponent } from "src/app/shared/components/button/button.component";
 import { IonInput } from "@ionic/angular/standalone";
+import { Login } from 'src/app/core/models/auth/login';
+import { ReactiveFormsModule, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-login',
   templateUrl: './form-login.component.html',
   styleUrls: ['./form-login.component.scss'],
-  imports: [ButtonComponent, IonInput],
+  imports: [ButtonComponent, IonInput,ReactiveFormsModule],
 })
-export class FormLoginComponent  implements OnInit {
+export class FormLoginComponent implements OnInit {
+  formAuth = output<Login>()
 
-  constructor() { }
+  formulario !: FormGroup
+  form = inject(FormBuilder)
+  constructor() {
+    this.formulario = this.form.group({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required])
+    });
+  }
 
-  ngOnInit() {}
+  ngOnInit() { }
+
+  submitForm() {
+    if(this.formulario.valid){
+      //alert("Formulario enviado correctamente");
+      const loginData: Login = {
+        email: this.formulario.controls['email'].value,
+        password: this.formulario.controls['password'].value
+      };
+      this.formAuth.emit(loginData);
+    }else{
+      alert("Por favor, complete todos los campos requeridos.");
+    }
+  }
 
 }
--- src/app/pages/auth/login/login.page.ts
+++ src/app/pages/auth/login/login.page.ts
@@ -1,22 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { CardComponent } from "src/app/shared/components/card/card.component";
 import { SubtitleComponent } from "src/app/shared/components/subtitle/subtitle.component";
 import { FormLoginComponent } from "src/app/components/auth/form-login/form-login.component";
+import { Login } from 'src/app/core/models/auth/login';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [ CommonModule, FormsModule, CardComponent, SubtitleComponent, FormLoginComponent]
+  imports: [CommonModule, FormsModule, CardComponent, SubtitleComponent, FormLoginComponent]
 })
 export class LoginPage implements OnInit {
-
+  private router = inject(Router)
   constructor() { }
 
   ngOnInit() {
   }
 
+  autenticacion(data: Login) {
+    console.log(data);
+    this.router.navigate(['/home/history-paciente']);
+  }
 }
