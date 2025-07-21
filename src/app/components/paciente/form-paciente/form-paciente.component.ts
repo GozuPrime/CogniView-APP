72386306302c479beaff597e85ebbba41f34586b
Gozu <rbazalarca11@ucvvirtual.edu.pe>
2025-07-21 12:30:23 -0500

    Implementación form-paciente

--- src/app/components/paciente/form-paciente/form-paciente.component.ts
+++ src/app/components/paciente/form-paciente/form-paciente.component.ts
@@ -1,17 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit, output } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, FormControl, FormGroup, Validator, Validators} from '@angular/forms';
 import { IonInput } from "@ionic/angular/standalone";
+import { Paciente } from 'src/app/core/models/paciente/paciente';
 import { ButtonComponent } from "src/app/shared/components/button/button.component";
 
 @Component({
   selector: 'app-form-paciente',
   templateUrl: './form-paciente.component.html',
   styleUrls: ['./form-paciente.component.scss'],
-  imports: [IonInput, ButtonComponent],
+  imports: [IonInput, ButtonComponent, ReactiveFormsModule],
 })
 export class FormPacienteComponent  implements OnInit {
+  formAuth = output<Paciente>()
 
-  constructor() { }
+  formulario !: FormGroup
+  form = inject(FormBuilder)
+  constructor() { 
+    this.formulario = this.form.group({
+      nombre: new FormControl('', [Validators.required]),
+      apellido: new FormControl('', [Validators.required]),
+      dni: new FormControl('', [Validators.required, Validators.pattern('^[0-9]+$')]),
+    });
+  }
 
   ngOnInit() {}
+
+  submitForm() {
+    if(this.formulario.valid){
+      //alert("Formulario enviado correctamente");
+      const pacienteData: Paciente = {
+        idPaciente: this.formulario.controls['idPaciente']?.value || 0,
+        idUsuario: this.formulario.controls['idUsuario']?.value || 0,
+        nombre: this.formulario.controls['nombre'].value,
+        apellido: this.formulario.controls['apellido'].value,
+        dni: this.formulario.controls['dni'].value
+      };
+      this.formAuth.emit(pacienteData);
+    }else{
+      alert("Por favor, complete todos los campos requeridos.");
+    }
+  }
 
 }
