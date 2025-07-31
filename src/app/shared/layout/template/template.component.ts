32005079dc3ef8f95cabe0536393dddcb162051f
Anthony Enrique Velasquez Honorio <anthony2000_honorio@outlook.com>
2025-07-31 13:51:22 -0500

    complete close sesion

--- src/app/shared/layout/template/template.component.ts
+++ src/app/shared/layout/template/template.component.ts
@@ -1,20 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { IonIcon, IonTabBar, IonTabButton, IonTabs, IonContent, IonMenu } from '@ionic/angular/standalone';
 
 import { addIcons } from 'ionicons';
 import { peopleOutline, clipboardOutline } from 'ionicons/icons';
 import { HeaderComponent } from "../../components/header/header.component";
+import { ImagenComponent } from "../../components/imagen/imagen.component";
+import { ButtonComponent } from "../../components/button/button.component";
+import { AuthService } from 'src/app/core/services/auth.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-template',
   templateUrl: './template.component.html',
   styleUrls: ['./template.component.scss'],
-  imports: [IonIcon, IonTabBar, IonTabButton, IonTabs, IonContent, IonMenu, HeaderComponent]
+  imports: [IonIcon, IonTabBar, IonTabButton, IonTabs, IonContent, IonMenu, HeaderComponent, ImagenComponent, ButtonComponent]
 })
 export class TemplateComponent {
+
+  private authServices = inject(AuthService)
+  private router = inject(Router)
 
   constructor() {
     addIcons({ peopleOutline, clipboardOutline });
   }
 
-
+  cerrarSesion() {
+    this.authServices.logout()
+    this.router.navigate(['/login'])
+  }
 }
