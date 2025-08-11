92de70f0507b716c135735b48bf6e16334032eb2
Anthony Enrique Velasquez Honorio <anthony2000_honorio@outlook.com>
2025-08-10 22:30:06 -0500

    margenes

--- src/app/shared/layout/template/template.component.ts
+++ src/app/shared/layout/template/template.component.ts
@@ -7,6 +7,7 @@ import { ImagenComponent } from "../../components/imagen/imagen.component";
 import { ButtonComponent } from "../../components/button/button.component";
 import { AuthService } from 'src/app/core/services/auth.service';
 import { Router } from '@angular/router';
+import { UtilsService } from 'src/app/core/services/utils.service';
 @Component({
   selector: 'app-template',
   templateUrl: './template.component.html',
@@ -17,13 +18,14 @@ export class TemplateComponent {
 
   private authServices = inject(AuthService)
   private router = inject(Router)
-
+  private utilService = inject(UtilsService)
   constructor() {
     addIcons({ peopleOutline, clipboardOutline });
   }
 
   cerrarSesion() {
     this.authServices.logout()
+    this.utilService.dismissModal()
     this.router.navigate(['/login'])
   }
 }
